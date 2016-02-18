22fe6d3cf6a567b346ca744d6d4bce410b512684
wasimakram-mulla <wasim3ace@gmail.com>
2016-02-18 19:18:07 +0530

    Login Successfully done

--- admin/checklogin.php
+++ admin/checklogin.php
@@ -1,12 +1,12 @@
 <?php
 $con= mysql_connect('localhost','root','')
         or die('can not connect');
-$db=  mysql_select_db('logindb',$con)
+$db=  mysql_select_db('gto_iams',$con)
        or die('can not connect to database'. mysql_error());
 	   
 	$unm=$_GET['usernm'];
 	$pwd=$_GET['passwd'];
-	$qry="select * from  login_master  where username='".$unm."' and password='".$pwd."'";
+	$qry="select * from  user_master  where username='".$unm."' and password='".$pwd."'";
 	$result =  mysql_query($qry);
 	$count =  mysql_num_rows($result);	
 	if($count>0){
